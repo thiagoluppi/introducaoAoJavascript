3683b8afc5839921c339c7b06144babb4546ea4d
thiagoluppi <thiago.luppi@icloud.com>
2021-11-13 20:36:06 -0300

    commit aula_operadores

---
+++ operadores.js
@@ -0,0 +1,31 @@
+// operadores
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+
+// comparação
+
+// igual
+console.log(2 == 2);
+console.log(2 == 3);
+
+// diferente
+console.log(2 != 2);
+
+// Igual estrito (retorna true se o valor e o tipo forem iguais)
+// console.log(2 === "2") => false => pois o tipo é diferente
+// console.log(2 == "2") => true => apesar dos tipos serem diferentes, numa comparação não estrita isso não importa
+console.log(2 === "2");
+console.log(2 == "2");
+
+// diferente estrito:
+console.log(3 !== "3");
+
+// maior ou menor:
+console.log(2 > 3);
+console.log(2 < 3);
+
+// maior ou igual:
+console.log(2 >= 2);
+console.log(2 <= 2);
---
+++ variaveis.js
@@ -0,0 +1,14 @@
+// O JS também usa tipagem dinamica como o Ruby:
+var number = 10;
+console.log(number)
+
+// para trazer o tipo da variavel, igual o .class do Ruby:
+console.log(typeof number);
+
+var nome = "Thiago";
+console.log(nome);
+console.log(typeof nome);
+
+var numero1 = 10;
+var numero2 = 20;
+console.log(numero1 + numero2);
