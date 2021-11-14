d6da335d4bce4bb0d5a17043a97191c1d6140f17
thiagoluppi <thiago.luppi@icloud.com>
2021-11-14 17:08:23 -0300

    commit aula_funcoes

--- main.js
+++ main.js
@@ -1,38 +1,15 @@
-// vamos recortar o codigo do script e vamos colocar ele dentro desse arquivo
-var welcome = "Hello JavaScript - Automação JS"
-document.getElementById("Welcome").append(welcome);
-// console.log é tipo o puts do Ruby:
-console.log(welcome);
+function welcome() {
+  var welcome = "Hello JavaScript - Automação JS"
+  document.getElementById("Welcome").append(welcome);
+  console.log(welcome);
+}
 
+welcome();
 
-// var avengers = new Array(); ou então fazer mais simples como abaixo:
-var avengers = ["idiota", "fantasioso", "sem-graça"];
+function soma(n1, n2) {
+  return n1 + n2;
+}
 
-console.log(avengers);
+total = soma(10, 10);
 
-avengers.push("otario-verde");
-
-console.log(avengers);
-
-// remover o primeiro e o ultimo registro de um array:
-avengers.push("aranha lixo");
-console.log(avengers);
-// avengers.pop(); // remove o ultimo registro do array
-// avengers.shift(); // remove o primeiro registro do array
-
-// para remover apenas um registro distinto, como aranha lixo:
-// descobrir sua posicao:
-var indice = avengers.indexOf("aranha lixo");
-console.log(indice);
-console.log(avengers);
-
-// agora fazer um splice na posicao. O metodo splice remove.
-avengers.splice(indice);
-
-console.log(avengers);
-
-// para concatenar dois arrays:
-var newAvengers = ["mais do mesmo", "já deu", "quem ainda ve essa merda?"]
-
-var result = avengers.concat(newAvengers);
-console.log(result)
+console.log(total);
