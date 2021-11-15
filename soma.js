63f68aa83d1ee1916618596880332660eea96a64
thiagoluppi <thiago.luppi@icloud.com>
2021-11-15 10:29:00 -0300

    commit aula_document_object_model

---
+++ soma.js
@@ -0,0 +1,10 @@
+function somaValores() {
+  var n1 = document.getElementById("numberOne").value;
+  var n2 = document.getElementById("numberTwo").value;
+
+  var result = parseInt(n1) + parseInt(n2);
+  var divResultado = document.getElementById("resultado");
+
+  divResultado.append(`O resultado da soma é ${result}`);
+  console.log(result);
+}
